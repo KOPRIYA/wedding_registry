f68fa14f7af019a9fe095a170e996493a73ad78d
Komal <infokomalpriya@gmail.com>
2026-09-16 14:56:48 +0530

    Update registry backend URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.WEDDING_REGISTRY_API_URL = "https://script.google.com/macros/s/AKfycbw8LOuau6Yu5q5DD3cguPk9rkVM5dIpqXBz5VjzZWKn7NAGMDfiQLRtzrlvsPC2qA7cIw/exec";
+window.WEDDING_REGISTRY_API_URL = "https://script.google.com/macros/s/AKfycbyfWkLuW29DHn0AWqueG9Y8zyurJ5t_AQj2FdGPjPesN7RJteyuUpRqdvwD5KIb-cYjhQ/exec";
 window.WEDDING_REGISTRY_ADMIN_KEY = "";
 window.WEDDING_REGISTRY_API_TRANSPORT = "jsonp";
