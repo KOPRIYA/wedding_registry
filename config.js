c683fa204dfc182b4708dde6a49a46f03acb0ba1
Komal <infokomalpriya@gmail.com>
2026-09-16 14:19:28 +0530

    Use dist directory for Sites hosting

---
+++ config.js
@@ -0,0 +1,3 @@
+window.WEDDING_REGISTRY_API_URL = "https://script.google.com/macros/s/AKfycbw8LOuau6Yu5q5DD3cguPk9rkVM5dIpqXBz5VjzZWKn7NAGMDfiQLRtzrlvsPC2qA7cIw/exec";
+window.WEDDING_REGISTRY_ADMIN_KEY = "";
+window.WEDDING_REGISTRY_API_TRANSPORT = "jsonp";
