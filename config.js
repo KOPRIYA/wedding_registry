5bf22c6f930fd67776caaaf94fe8d751458fc942
Komal <infokomalpriya@gmail.com>
2026-09-16 14:39:29 +0530

    Surface registry storage connection errors

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.WEDDING_REGISTRY_API_URL = "https://script.google.com/macros/s/AKfycbzfUtPkcRGjhh5uTjksQ07xx0ArMN-sxJdzZRhxVWlFqsWddBV5rQsKqv8qHVmyH5byrw/exec";
+window.WEDDING_REGISTRY_API_URL = "https://script.google.com/macros/s/AKfycbw8LOuau6Yu5q5DD3cguPk9rkVM5dIpqXBz5VjzZWKn7NAGMDfiQLRtzrlvsPC2qA7cIw/exec";
 window.WEDDING_REGISTRY_ADMIN_KEY = "";
 window.WEDDING_REGISTRY_API_TRANSPORT = "jsonp";
